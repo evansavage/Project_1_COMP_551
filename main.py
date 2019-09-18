24d8db1ce903b8be863af86a0746d7ba2eec8238
caxon <caxon26@gmail.com>
2019-09-18 18:45:33 -0400

    started evaluate_acc function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import dataset_load
 import logistic_regression
 import linear_discriminant_analysis
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def evaluate_acc(data_points, true_lables, target_lables):
+  if not isinstance(data_points, np.array):
+    raise "Data points must be of type numpy array"
+  if not isinstance(true_lables, np.array):
+    raise "Data points must be of type numpy array"
+  if not isinstance(target_lables, np.array):
+    raise "Data points must be of type numpy array"
+
