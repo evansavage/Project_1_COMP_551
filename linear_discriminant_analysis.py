0c2e70846f141a11aa43e1e3279b8173a603355d
evansavage <esav678@gmail.com>
2019-09-18 21:01:03 -0400

    Wrote initial classes for LR and LDA

--- linear_discriminant_analysis.py
+++ linear_discriminant_analysis.py
@@ -1,6 +1,11 @@
-class LinearDiscriminatAnalysis:
-    def __init__(self):
-        print()
+import numpy as np
+
+class LinearDiscriminantAnalysis:
+    def __init__(self, dataset):
+        dataset = np.asarray(dataset)
+        self.dataset = dataset[:, :-1]
+        self.labels = dataset[:, -1]
+        self.iter = 1000
 
     def fit(self):
         return
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,9 +1,26 @@
-class LogisticRegression:
-    def __init__(self):
-        print()
+import math
+import numpy as np
+
+
+class LogisticRegression(object):
+    def __init__(self, dataset):
+        dataset = np.asarray(dataset)
+        self.dataset = dataset[:, :-1]
+        self.labels = dataset[:, -1]
+        self.iter = 1000
 
     def fit(self):
         return
 
     def predict(self):
         return
+
+
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+
+lr = LogisticRegression([[1,2,3],[4,5,6],[7,8,9]])
+print(lr.dataset)
+print(lr.labels)
