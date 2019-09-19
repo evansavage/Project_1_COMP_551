752efd07611ccc6ecaebae5752eb80e2a28602f4
evansavage <esav678@gmail.com>
2019-09-18 21:03:31 -0400

    Re-added evaluate accuracy function (oops!)

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ lr_cancer = LogisticRegression(breast_cancer_dataset)
 
 lda_wine = LinearDiscriminantAnalysis(wine_dataset)
 lda_cancer = LinearDiscriminantAnalysis(breast_cancer_dataset)
+
+def evaluate_acc(data_points, true_lables, target_lables):
+  if not isinstance(data_points, np.array):
+    raise "Data points must be of type numpy array"
+  if not isinstance(true_lables, np.array):
+    raise "Data points must be of type numpy array"
+  if not isinstance(target_lables, np.array):
+    raise "Data points must be of type numpy array"
