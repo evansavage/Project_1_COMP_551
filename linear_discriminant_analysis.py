b0c356049d206ea57caf4c2158f3c9f49e337f4b
evansavage <esav678@gmail.com>
2019-09-18 22:23:18 -0400

    Changed class attribute names

--- linear_discriminant_analysis.py
+++ linear_discriminant_analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 class LinearDiscriminantAnalysis:
     def __init__(self, dataset):
         dataset = np.asarray(dataset)
-        self.dataset = dataset[:, :-1]
+        self.features = dataset[:, :-1]
         self.labels = dataset[:, -1]
         self.iter = 1000
 
