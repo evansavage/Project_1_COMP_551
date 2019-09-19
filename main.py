6d214581f3ad6a85f3b4da050eda267a670b43c8
evansavage <esav678@gmail.com>
2019-09-18 21:01:14 -0400

    Added imports and external function calls

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import dataset_load
-import logistic_regression
-import linear_discriminant_analysis
+from dataset_load import load_dataset, clean_dataset_nan
+from logistic_regression import LogisticRegression
+from linear_discriminant_analysis import LinearDiscriminantAnalysis
+
+wine_dataset = load_dataset('winequality-red.csv', ';')
+breast_cancer_dataset = load_dataset('breast-cancer-wisconsin.data', ',')
+
+wine_dataset = clean_dataset_nan(wine_dataset)
+breast_cancer_dataset = clean_dataset_nan(breast_cancer_dataset)
+
+lr_wine = LogisticRegression(wine_dataset)
+lr_cancer = LogisticRegression(breast_cancer_dataset)
+
+lda_wine = LinearDiscriminantAnalysis(wine_dataset)
+lda_cancer = LinearDiscriminantAnalysis(breast_cancer_dataset)
+
+
+# print(lr_breast_cancer.dataset)
+# print(lr_wine.labels)
