8867d57d048b2c5ec694bbf15cc881c311032fa4
evansavage <esav678@gmail.com>
2019-09-18 22:24:10 -0400

    Added class definitions for logistic regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,24 +3,49 @@ import numpy as np
 
 
 class LogisticRegression(object):
-    def __init__(self, dataset):
+    def __init__(self, dataset, iter, learning_rate):
         dataset = np.asarray(dataset)
-        self.dataset = dataset[:, :-1]
+        self.features = dataset[:, :-1]
         self.labels = dataset[:, -1]
-        self.iter = 1000
+        self.iter = iter
+        self.learning_rate = learning_rate
+
+    def threshold(self, value):
+        for i in range(0, len(self.labels)-1):
+            if self.labels[i] > value:
+                self.labels[i] = 1
+            else:
+                self.labels[i] = 0
 
     def fit(self):
-        return
+        N = len(self.labels)
+        for i in range(0, N-1):
+            print(self.labels[i])
+
+    def update_coefficients(self):
+        w = [0 for i in range(len(self.features[0]) + 1)]
+        for i in range(self.iter):
+            for j, row in enumerate(self.features):
+                sigma = sigmoid(row, w)
+                error = self.labels[j] - sigma
+                w[0] = w[0] + self.learning_rate * error * sigma * (1 - sigma)
+                for k in range(1, len(row)):
+                    w[k] = w[k] + self.learning_rate * error * sigma * (1 - sigma) * row[k-1]
+
+        return w
 
     def predict(self):
         return
 
 
 
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+def sigmoid(x, w):
+    a = w[0]
+    for i in range(len(x)):
+        a += w[i+1]*x[i]
+    return 1 / (1 + math.exp(-a))
 
 
-lr = LogisticRegression([[1,2,3],[4,5,6],[7,8,9]])
-print(lr.dataset)
-print(lr.labels)
+# lr = LogisticRegression([[1,2,3],[4,5,6],[7,8,9]])
+# print(lr.dataset)
+# print(lr.labels)
