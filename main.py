9a28ede69ef696b81793e0b04a8efb6838f2f25c
caxon <caxon26@gmail.com>
2019-09-22 15:04:26 -0400

    almost finished kfold in main.py

--- main.py
+++ main.py
@@ -29,22 +29,49 @@ print(lr_cancer.update_coefficients())
 # lr_cancer.threshold(3)
 # lr_cancer.fit()
 
-def evaluate_acc(data_points, true_lables, target_lables):
-  # make sure data input in correct types (np.arrays)
-  if not isinstance(data_points, np.array):
-    raise "Data points must be of type numpy array"
-  if not isinstance(true_lables, np.array):
-    raise "Data points must be of type numpy array"
-  if not isinstance(target_lables, np.array):
-    raise "Data points must be of type numpy array"
+def evaluate_acc( true_lables, target_lables):
+  """true labels and target labels should both be numpy column arrays of size n*1"""
 
-  count = data_points.shape()[0]
+  sample_size = data_points.shape[0]
 
 
 
+x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]])
+y = np.array([101,102,103,104,105, 106, 107, 108, 109, 110]).reshape(-1,1)
 
 
-def k_fold_cross_validation(k, dataset, model):
+def k_fold_cross_validation(k, X, Y, model):
+  """Divides dataset X and labels Y into k different bins and runs k-fold cross validation.
+  @returns: the average accuracy of the k folds"""
 
-  training_data = 0;
-  validation_data = 0;
+  assert X.shape[0] == Y.shape[0] # ensure that the number of features in X matches Y
+  sample_size = X.shape[0]
+  fold_size = sample_size // k
+  remainder = sample_size % k
+
+  # Calculate the size of each of the k chunks into inclusive sliced ranges e.g. [s0, s1]
+  slices = []
+  for i in range(k):
+    if i == 0:
+      slices.append((0,fold_size  + (i < remainder) - 1))
+    else:
+      slices.append(( slices[i-1][1] + 1, slices[i-1][1] +  fold_size + (i < remainder)))
+
+  total_error = 0
+
+  # Break the data up into chunks for each of the k tests
+  #-----------------------------------------------------
+  for fold in range(k):
+    print("====== Kfold set " + str(fold)+ " =========")
+    training_data_1 = x[0 : slices[fold][0], :] # from first example to start of validation set
+    training_labels_1 = y[0 : slices[fold][0], :] # "" for labels
+    training_data_2 = x[slices[fold][1] + 1:, :] #from end of validation set to end of entire set
+    training_labels_2 = y[slices[fold][1] + 1:, :]
+
+    # Concatinate two halves of training data
+    training_data = np.vstack((training_data_1, training_data_2))
+    training_labels = np.vstack((training_labels_1, training_labels_2))
+    # get validaiton data
+    validation_data = x[slices[fold][0] : slices[fold][1] + 1, :]
+    validation_labels = y[slices[fold][0] : slices[fold][1] + 1, :]
+
