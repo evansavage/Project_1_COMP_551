dcd5e2d2ed2812d37a4c90939a3c88871ab4f173
Michel Kassis <michel.kassis120@gmail.com>
2019-09-22 16:39:12 -0400

    lda fit and predict setup

--- linear_discriminant_analysis.py
+++ linear_discriminant_analysis.py
@@ -8,16 +8,29 @@ class LinearDiscriminantAnalysis:
         self.iter = 1000
         self.learning_rate = 0.01
 
-    def fit(self, X, Y):
+    def fit(self, X:np.array, Y:np.array):
         """ @Params:
             -- X: dataset
             -- Y: labels
             @return:
             -- None (fits internal perameters to model)"""
+        # 1- Compute the total mean vector mu and the mean vector for each class muc(d dimensions)
+        mu = np.mean(X, axis=0).values
+        mu_k = []
+i
+        # 2- Compute the scatter matrices (in between and within class)
+        # within_class_scatter=  sum (scatter_per_class)
+        # scatter_per_class = sum((x-mi)(x-mi). T)
+        # 3- Compute the eigenvectors and eigenvalues for scatter matrices
+
+        # 4- Select the EigenVectors of the cooresponding k largest eigenvalues to create d*k matrix w
+
+        # 5- Use matrix w to transform n*d dataset x into lower n*k dataset y
+        return None
 
         return None
 
-    def predict(self, X_new):
+    def predict(self, X_new:np.array):
         """@Params:
             -- X_new: dataset
             @return:
