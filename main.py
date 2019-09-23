0acf598e376a81877a5f1b41a7aeabc3cc4f650d
evansavage <esav678@gmail.com>
2019-09-23 17:42:55 -0400

    Main function calls for LDA and LR

--- main.py
+++ main.py
@@ -16,35 +16,32 @@ breast_cancer_dataset = load_dataset('breast-cancer-wisconsin.data', ',', 1)
 wine_dataset = clean_dataset_nan(wine_dataset)
 breast_cancer_dataset = clean_dataset_nan(breast_cancer_dataset)
 
-## testing data
-# lr_wine = LogisticRegression(wine_dataset, 100, 0.3)
-# lr_cancer = LogisticRegression(breast_cancer_dataset, 1000, 0.01)
-# lr_wine.threshold(5)
-# lr_wine.normalize()
-# lr_wine.show()
-
-# lda_wine = LinearDiscriminantAnalysis(wine_dataset)
-# lda_cancer = LinearDiscriminantAnalysis(breast_cancer_dataset)
-# print(breast_cancer_dataset)
-
-# w = lr_wine.update_coefficients()
-# lr_wine.check_temp(w)
-#
-#
-# lr_cancer.threshold(3)
-# lr_cancer.normalize()
-# lr_cancer.update_coefficients()
-
-# lr_cancer.threshold(3)
-# lr_cancer.fit()
-
-
 ## TESTING DATA TODO: Comment out and remove before submitting
-x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]])
-y = np.array([1,1,103,104,105, 106, 107, 108, 109, 110]).reshape(-1,1)
+# x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]])
+# y = np.array([1,1,103,104,105, 106, 107, 108, 109, 110]).reshape(-1,1)
 
-wine_LR = LogisticRegression(3000, 0.01)
+X_wine = wine_dataset[:,:-1]
+Y_wine = wine_dataset[:, -1].reshape(-1,1)
+
+X_cancer = breast_cancer_dataset[:,:-1]
+Y_cancer = breast_cancer_dataset[:,-1].reshape(-1,1)
+
+X_wine2 = wine_dataset[:,:-1]
+Y_wine2 = wine_dataset[:, -1].reshape(-1,1)
+
+X_cancer2 = breast_cancer_dataset[:,:-1]
+Y_cancer2 = breast_cancer_dataset[:,-1].reshape(-1,1)
+
+wine_LR = LogisticRegression(1000, 0.3)
 cancer_LR = LogisticRegression(2000, 0.3)
-k_fold_cross_validation(5, wine_dataset[:,:-1], wine_dataset[:, -1].reshape(-1,1), wine_LR, 5, 'max')
-# k_fold_cross_validation(5, breast_cancer_dataset[:,:-1], breast_cancer_dataset[:,-1].reshape(-1,1), cancer_LR, 3, '')
+
+k_fold_cross_validation(5, X_wine, Y_wine, wine_LR, 5, '')
+k_fold_cross_validation(5, X_cancer, Y_cancer, cancer_LR, 3, '')
+
+wine_LDA = LinearDiscriminantAnalysis()
+cancer_LDA = LinearDiscriminantAnalysis()
+
+k_fold_cross_validation(5, X_wine2, Y_wine2, wine_LDA, 5, '')
+k_fold_cross_validation(5, X_cancer2, Y_cancer2, cancer_LDA, 3, '')
+
 print("DONE")
