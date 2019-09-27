f85ffb81cf28490ce5b7e6e26e43f592a932248c
evansavage <esav678@gmail.com>
2019-09-27 18:05:04 -0400

    Changed import configuration

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import numpy as np
+import pandas as pd
 
 # import files from split project
-from dataset_load import load_dataset, clean_dataset_nan, visualize_dataset
+from dataset_load import load_dataset
 from logistic_regression import LogisticRegression
 from linear_discriminant_analysis import LinearDiscriminantAnalysis
 from cross_validation import evaluate_acc, k_fold_cross_validation
 
 # load datasets
-wine_dataset = load_dataset('winequality-red.csv', ';', [])
-breast_cancer_dataset = load_dataset('breast-cancer-wisconsin.data', ',', [1])
+wine_dataset = load_dataset('winequality-red.csv',
+    ';',
+    # interaction=[['citric acid', 'fixed acidity'], ['density', 'fixed acidity']]
+    )
+breast_cancer_dataset = load_dataset('breast-cancer-wisconsin.data',
+    ',',
+    # visualize=True,
+    # interaction=[['Uniformity of Cell Shape', 'Uniformity of Cell Size']],
+    remove_columns=['Sample code number'])
 
+# wine_dataset_test = pd.read_csv('winequality-red.csv', ';')
+# print(wine_dataset_test.iloc[:, -1])
+# breast_cancer_dataset_test = pd.read_csv('breast-cancer-wisconsin.data', ',')
+# print(breast_cancer_dataset_test.iloc[:,-1])
 # data clearning
-wine_dataset = clean_dataset_nan(wine_dataset)
-breast_cancer_dataset = clean_dataset_nan(breast_cancer_dataset)
-
-# visualize_dataset('winequality-red.csv', ';', columns=(0,1,2,6,5))
-# visualize_dataset('breast-cancer-wisconsin.data', ',',columns=(1,2))
+# wine_dataset = clean_dataset_nan(wine_dataset)
+# breast_cancer_dataset = clean_dataset_nan(breast_cancer_dataset)
 
 ## TESTING DATA TODO: Comment out and remove before submitting
 # x = np.array([[1,2],[3,4],[5,6],[7,8],[9,10],[11,12],[13,14],[15,16],[17,18],[19,20]])
@@ -26,23 +35,27 @@ Y_wine = wine_dataset[:, -1].reshape(-1,1)
 
 X_cancer = breast_cancer_dataset[:,:-1]
 Y_cancer = breast_cancer_dataset[:,-1].reshape(-1,1)
-
+#
 X_wine2 = X_wine.copy()
 Y_wine2 = Y_wine.copy()
 
 X_cancer2 = X_cancer.copy()
 Y_cancer2 = Y_cancer.copy()
-
-wine_LR = LogisticRegression(100, 0.3)
-cancer_LR = LogisticRegression(100, 0.3)
-
-# k_fold_cross_validation(5, X_wine, Y_wine, wine_LR, 5.5, '', True, False)
+#
+wine_LR = LogisticRegression(100, 0.2)
+cancer_LR = LogisticRegression(1200, 0.5)
+#
+print('*** WINE LR ***')
+k_fold_cross_validation(5, X_wine, Y_wine, wine_LR, 5.5, '', True, False)
+print('*** CANCER LR ***')
 # k_fold_cross_validation(5, X_cancer, Y_cancer, cancer_LR, 3, '', True, False)
+# #
+wine_LDA = LinearDiscriminantAnalysis()
+cancer_LDA = LinearDiscriminantAnalysis()
 #
-# wine_LDA = LinearDiscriminantAnalysis()
-# cancer_LDA = LinearDiscriminantAnalysis()
+print('*** WINE LDA ***')
+k_fold_cross_validation(5, X_wine2, Y_wine2, wine_LDA, 5.5, '', True, False)
+print('*** CANCER LDA ***')
+k_fold_cross_validation(5, X_cancer2, Y_cancer2, cancer_LDA, 3, '', True, False)
 #
-# k_fold_cross_validation(5, X_wine2, Y_wine2, wine_LDA, 5, '', True, False)
-# k_fold_cross_validation(5, X_cancer2, Y_cancer2, cancer_LDA, 3, '', True, False)
-
-print("DONE")
+# print("DONE")
